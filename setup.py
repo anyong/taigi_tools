cefc94b548286447400754adaff4046463bca5fd
Edward Greve <edward.greve@gmail.com>
2013-12-26 02:32:21 +0800

    basic file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='taigi_tools',
+    version='0.1',
+    description='Tools for Taiwanese pinyin and character conversions',
+    url='',
+    author='Edward Greve',
+    author_email='edward.greve@gmail.com',
+    license='MIT',
+    packages=['taigi_tools'],
+    zip_safe=False)
