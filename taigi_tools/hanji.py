6f6beb46012fd480044e667cc3a75de326e1b3d3
Edward Greve <edward.greve@gmail.com>
2013-12-30 03:24:20 +0800

    Basic hanji conversion (quite broken!)
    The Hanji to Tailo conversions are slightly better, but tailo to hanji
    conversions are not often successful, unless the exact phrasing happens
    to be in the MOEs dictionary that the data came from… can probably be
    improved a bit to at least be usable

--- taigi_tools/hanji.py
+++ taigi_tools/hanji.py
@@ -1,7 +1,25 @@
-import unicodedata
-import re
+import codecs
 
-DATFILE = 'tailo-hanji.dat'
+class Hanji:
+    DATFILE = 'TLHJ.DAT'
+    
+    def __init__(self):
+        f = codecs.open(self.DATFILE, 'r', 'utf-8')
+        self.lines = []
+        for l in f:
+            self.lines.append(l.rstrip())
+        self.lines.sort(key=len, reverse=True)
+        self.conversion_table = []
+        for l in self.lines:
+            self.conversion_table.append(l.split('\t'))
 
-def tailo_to_hanji(string):
+    def to_hanji(self, string):
+        for x in self.conversion_table:
+            string = string.replace(x[0], x[1])
+        return string
+    
+    def to_tailo(self, string):
+        for x in self.conversion_table:
+            string = string.replace(x[1], x[0]+' ')
+        return string
     
